c1e9448c00d8725d333ded5cc8e53cfb964202c3
Daniel <danielmichael.fabricius@gmail.com>
2022-11-03 17:46:55 +0100

    Added OR and XOR gates

---
+++ Assets/Scripts/OrGate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrGate : BinaryLogicGate
+{
+    protected override bool ComputeValue()
+    {
+        return input1.GetValue() | input2.GetValue();
+    }
+}
---
+++ Assets/Scripts/XorGate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XorGate : BinaryLogicGate
+{
+    protected override bool ComputeValue()
+    {
+        return input1.GetValue() ^ input2.GetValue();
+    }
+}
